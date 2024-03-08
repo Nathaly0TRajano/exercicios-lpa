cc2b2daa7369312e5d48ba53e87bdff7fcbf97c3
Nathaly0TRajano <nathydelima00@gmail.com>
2024-03-08 10:22:59 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,13 @@ Route::get("cadastro/usuario", function(Request $request){
     $Usuario = $request->input("Nome");
     return $Usuario;
 });
+
+Route::get("cadastro/dados", function(Request $request){
+
+    $Dado1 = $request->input("nome");
+    $Dado2 = $request->input("Idade");
+
+    return 'Meu nome é ' . $Dado1 . ' ,tenho ' . $Dado2 . ' ' . 'anos de idade';
+    
+
+});
